f09f13047a37c00c5c1724e54ab733b7ea55c1b5
Aayansh-singh-24 <luciferversion2.2@gmail.com>
2026-06-25 16:06:36 +0530

    Fix: Route analytics dashboard before default admin

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
-
+from users import views
 urlpatterns = [
     # The admin panel route
+    path('admin/analytics/', views.admin_analytics_dashboard, name='admin_analytics'),
     path('admin/', admin.site.urls),
     
     # This sends all homepage traffic to your users app
